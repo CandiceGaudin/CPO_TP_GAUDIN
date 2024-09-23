288bf72169b48fda8b4718bf91d2e404c44b0f02
kango <kango@10.3.60.67>
2024-09-23 18:50:30 +0200

    ManipNombresInt :

---
+++ TP1_GAUDIN/manipNombresInt/src/manipnombresint/ManipNombresInt.java
@@ -0,0 +1,37 @@
+/*
+ * Gaudin
+ * TP1 manipNombresInt
+ * TDA
+ */
+package manipnombresint;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kango
+ */
+public class ManipNombresInt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a;
+    int b;
+    int somme;
+    int produit;
+    int difference;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\n Entrer un entier :");
+    a=sc.nextInt();
+    Scanner sc1 = new Scanner(System.in);
+    System.out.println("\n Entrer un autre entier :");
+    b=sc1.nextInt();
+    somme=a+b;
+    produit=a*b;
+    difference=a-b;
+    System.out.println("\n Voici la somme: " + somme + ", la difference: " + difference + " et le produit: " + produit + " de a et b.");
+    }
+    
+}

3032c607439426edafe7429e2099b311e1d196e1
kango <kango@10.3.60.67>
2024-10-16 09:18:15 +0200

    TP2 fin de l'exo 1

--- TP2_GAUDIN/TP2_Bieres_GAUDIN/src/tp2_bieres_gaudin/TP2_Bieres_GAUDIN.java
+++ TP2_GAUDIN/TP2_Bieres_GAUDIN/src/tp2_bieres_gaudin/TP2_Bieres_GAUDIN.java
@@ -32,21 +32,31 @@ uneSecondeBiere.lireEtiquette();*/
 BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",
 7.0 ,"Dubuisson");
 uneBiere.lireEtiquette();
+uneBiere.Decapsuler();
+System.out.println(uneBiere) ;
 
 BouteilleBiere uneSecondeBiere = new BouteilleBiere("Leffe",    6.6, "Abbaye de Leffe") ;
 uneSecondeBiere.lireEtiquette();
+uneSecondeBiere.Decapsuler();
+System.out.println(uneSecondeBiere);
 
 BouteilleBiere BiereLambda = new BouteilleBiere("Biere",
 6.0 ,"Laboisson");
 BiereLambda.lireEtiquette();
+BiereLambda.Decapsuler();
+System.out.println(BiereLambda);
 
 BouteilleBiere autreautreBiere = new BouteilleBiere("Blonde",
 7.0 ,"Celle a cote de ta maison");
 autreautreBiere.lireEtiquette();
+autreautreBiere.Decapsuler();
+System.out.println(autreautreBiere);
 
 BouteilleBiere laDerniereBiere = new BouteilleBiere("Raiponse",
 7.5 ,"Le cannard boiteux");
-
+laDerniereBiere.lireEtiquette();
+laDerniereBiere.Decapsuler();
+ System.out.println(laDerniereBiere);
 
 
 

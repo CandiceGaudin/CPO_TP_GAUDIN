45d56243bf34625f4cb860c9bdc7e1589bbb86d9
kango <kango@10.3.60.67>
2024-10-16 09:18:38 +0200

    TP2 fin de l'exo 1

--- TP2_GAUDIN/TP2_Bieres_GAUDIN/src/tp2_bieres_gaudin/BouteilleBiere.java
+++ TP2_GAUDIN/TP2_Bieres_GAUDIN/src/tp2_bieres_gaudin/BouteilleBiere.java
@@ -14,11 +14,30 @@ public class BouteilleBiere {
     double degreAlcool;
     String brasserie;
     boolean ouverte;
+    public boolean Decapsuler() {
+        if (ouverte==false) {
+            ouverte = true;
+        }
+            else {
+                    System.out.println("erreur : biere déjà ouverte");
+                    }
+        return ouverte;
+        }
+    
    public void lireEtiquette() {
 System.out.println("Bouteille de " + Nom +" (" + degreAlcool +
  " degres) \nBrasserie : " + brasserie ) ;
 
    }
+   @Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = Nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
+   /* En dessous c'est le constructeur, sinon on peut faire boutton droit puis insert code pour faire la meme chose mais plus rapidement */
 public BouteilleBiere(String unNom, double unDegre, String
 uneBrasserie) {
  Nom = unNom;

9ebf43b8e5deb64344e24bcb2dd5954fb78b0e79
kango <kango@10.3.60.67>
2024-11-03 10:14:12 +0100

    TP2 l'exo 2 Convertisseur : créer deux objets qui effectue des conversions

--- TP2_GAUDIN/TP2_convertisseurObjet_GAUDIN/src/tp2_convertisseurobjet_gaudin/TP2_convertisseurObjet_GAUDIN.java
+++ TP2_GAUDIN/TP2_convertisseurObjet_GAUDIN/src/tp2_convertisseurobjet_gaudin/TP2_convertisseurObjet_GAUDIN.java
@@ -15,7 +15,29 @@ public class TP2_convertisseurObjet_GAUDIN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        double a;
+        double rep1 = 0;
+        double rep2 = 0;
+        double rep3 = 0;
+        double rep4 = 0;
+        System.out.println("\n Bonjour, saisissez une valeur : ");
+    Scanner sc = new Scanner(System.in);
+    a=sc.nextDouble();
     
+     Convertisseur Celcius_vers = new Convertisseur() ;
+Celcius_vers.CelciusVersKelvin(a) = rep1;
+Celcius_vers.CelciusVersFarenheit(a) = rep2;
+System.out.println("nombre de conversion = 2");
+
+// Je ne comprends pas pourquoi le logiciel veux une variable et pas une valeur...
+// J'ai essayé de faire en sorte que les deux objets créé effectuent chacun deux conversions,
+//et pour cela j'ai demandé a l'utilisateur de me donner une valeur que je pourrai donc inséré dans les fonctions et ainsi avoir un résultat
+
+Convertisseur Kelvin_vers = new Convertisseur() ;
+Kelvin_vers.KelvinVersFarenheit(a) = rep3;
+Kelvin_vers.KelvinVersCelcius(a) = rep4;
+System.out.println("nombre de conversion = 2");
+
     }
     
 }

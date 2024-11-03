f08bde97b0c3748a8263acbd8369b64463da683a
kango <kango@10.3.60.67>
2024-11-03 19:13:24 +0100

    TP2 l'exo 4 : travailler les relations entre objets

---
+++ TP2_GAUDIN/TP2_relation_1_GAUDIN/src/tp2_relation_1_gaudin/TP2_relation_1_GAUDIN.java
@@ -0,0 +1,44 @@
+/*
+ * Gaudin
+ * TP2
+ * relation 1
+ */
+package tp2_relation_1_gaudin;
+
+/**
+ *
+ * @author kango
+ */
+public class TP2_relation_1_GAUDIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ; 
+        Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ; 
+        Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        Personne bob = new Personne("Bobby", "Sixkiller");
+        Personne reno = new Personne("Reno", "Raines");
+        System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra); //affichage de toutes les voitures
+        bob.liste_voitures[0] = uneClio ;
+        bob.nbVoiture = 1 ;
+        uneClio.proprietaire = bob ;
+        System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
+        bob.liste_voitures[1] = une2008;
+        bob.nbVoiture = 2;
+        une2008.proprietaire = bob;
+        System.out.println("la deuxieme voiture de Bob est " +bob.liste_voitures[1] ) ;
+        reno.liste_voitures[0] = uneAutreClio;
+        reno.nbVoiture = 1;
+        uneAutreClio.proprietaire = reno;
+        System.out.println("la premiere voiture de Reno est " +reno.liste_voitures[0] );
+        uneMicra.proprietaire = reno;
+        reno.liste_voitures[1] = uneMicra;
+        reno.nbVoiture = 2;
+        uneMicra.proprietaire = reno;
+        System.out.println("la deuxieme voiture de Reno est " +reno.liste_voitures[1] );
+        
+    
+}

118e44cce3d303a3aaa5c1e96319caa8d31f12cc
kango <kango@10.3.60.67>
2024-11-06 12:17:51 +0100

    création TP3

---
+++ TP2_GAUDIN/TP3_Heroic_Fantasy_GAUDIN/src/tp3_heroic_fantasy_gaudin/TP3_Heroic_Fantasy_GAUDIN.java
@@ -0,0 +1,21 @@
+/*
+ * Gaudin
+ * TP2
+ * relation 1
+ */
+package tp3_heroic_fantasy_gaudin;
+
+/**
+ *
+ * @author kango
+ */
+public class TP3_Heroic_Fantasy_GAUDIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

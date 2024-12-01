d75432ca336ad027c8e8a045027ed1b33959a309
kango <kango@10.3.60.67>
2024-12-01 17:00:22 +0100

    TP3 : classes Personnages, rearengement des classes

--- TP2_GAUDIN/TP3_Heroic_Fantasy_GAUDIN/src/tp3_heroic_fantasy_gaudin/TP3_Heroic_Fantasy_GAUDIN.java
+++ TP2_GAUDIN/TP3_Heroic_Fantasy_GAUDIN/src/tp3_heroic_fantasy_gaudin/TP3_Heroic_Fantasy_GAUDIN.java
@@ -5,6 +5,12 @@
  */
 package tp3_heroic_fantasy_gaudin;
 import java.util.ArrayList;
+import Armes.Arme;
+import Armes.Baton;
+import Armes.Epee;
+import Personnages.Personnage;
+import Personnages.Magicien;
+import Personnages.Guerrier;
 
 /**
  *
@@ -20,15 +26,50 @@ public class TP3_Heroic_Fantasy_GAUDIN {
         Epee Durandal = new Epee (4,"Durandal", 7);
         Baton Chene = new Baton (4, "Chene", 5);
         Baton Charme = new Baton (5, "Charme", 6); 
+        Baton Saul = new Baton(6,"Saul", 9);
+        Epee Needle = new Epee (8,"Needle", 4);
         
-        ArrayList<Arme> Equipement = new ArrayList<Arme>();
+        /*ArrayList<Arme> Equipement = new ArrayList<Arme>();
         Equipement.add(Excalibur);
         Equipement.add(Durandal);
         Equipement.add(Chene);
         Equipement.add(Charme);
         
         int taille = Equipement.size();
-
+        for (int i=0 ; i<longueur ; i++) {
+            System.out.println("L'arme "+i+" est : "+armes.get(i));
+        }*/
+        
+          Magicien Magicien1 = new Magicien("Gandalf", 65, true);
+        /*Magicien Magicien2 = new Magicien("Garcimore", 44, false);
+        Guerrier Guerrier1 = new Guerrier("Conan", 75, false); */
+        Guerrier Guerrier2 = new Guerrier("Lannister", 45, true);
+        /*ArrayList<Personnage> personnage = new ArrayList<Personnage>();
+        personnage.add(Guerrier1);
+        personnage.add(Guerrier2);
+        personnage.add(Magicien1);
+        personnage.add(Magicien2);
+        int taille = personnage.size();
+        for (int j=0; j<taille; j++) {
+            System.out.println("Le personnage "+j+" est : "+personnage.get(j));
+        }*/
+        Guerrier2.ajout_Arme(Excalibur);
+        Guerrier2.ajout_Arme(Charme);
+        Guerrier2.ajout_Arme(Needle);
+        Magicien1.ajout_Arme(Durandal);
+        Magicien1.ajout_Arme(Chene);
+        Magicien1.ajout_Arme(Saul);
+        System.out.println(Magicien1.Sac_d_Armes.size());
+        System.out.println(Magicien1);
+        int taille1=Magicien1.Sac_d_Armes.size();
+        for (int i=0;i>taille1 ;i++){
+            System.out.println(Magicien1.Sac_d_Armes.get(i));
+        }
+        System.out.println(Guerrier2);
+       
+       
+       
+       
     }
     
 }
